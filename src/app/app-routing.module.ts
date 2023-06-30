3cb99038742d2359ba67d4649d20c0cc8c2d6c74
taisbelucik <taisbelucik@hotmail.com>
2023-06-30 13:53:45 -0300

    Criando a interface HTML

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TarefaRoutes } from './tarefas/listar/tarefas-routing.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full',
+  },
+  ...TarefaRoutes,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
